cd8ca9d93bf9f1e9e8a822ae81a29e73d139595b
qiweidong <1512329942@qq.com>
2020-05-27 00:38:58 +0800

    2020-5-26：Dao层-新增店铺

---
+++ src/main/java/com/se/dao/ShopDao.java
@@ -0,0 +1,18 @@
+package com.se.dao;
+
+import com.se.domain.Shop;
+
+/**
+ * @Author: Qi Weidong
+ * @Description: 店铺
+ * @Date: Create in 23:43 2020/5/26
+ * @Modified by:
+ */
+public interface ShopDao {
+    /**
+     * 新增店铺
+     * @param shop
+     * @return
+     */
+    int insertShop(Shop shop);
+}
